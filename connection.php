0a4bc047596525c917001e370ecc2d137bcc5993
Kavindu Ariyasena <127376808+KavinduAriyasena@users.noreply.github.com>
2023-04-06 10:42:54 +0530

    wada krnne na ykoo

---
+++ connection.php
@@ -0,0 +1,24 @@
+<?php 
+$name = $_POST['username'];
+$email = $_POST['useremail'];
+$password = $_POST['userpassword'];
+
+//encrypt password
+
+$hashed_password = sha1($password);
+
+//database connection
+
+$conn = new mysqli('localhost','root','','sdgp');
+if($conn->connect_error){
+    die('Connection Failed :'. $conn->connect_error);
+}else{
+    $stmt = $conn->prepare("INSERT INTO registration(user_name,user_email,user_password)
+        values('?','?','?')");
+    $stmt->bind_param("sss",$name,$email,$hashed_password);
+    $stmt ->execute();
+    echo "registration successfully...";
+    $stmt->close();
+    $conn->close();
+}
+?>
